95560ca8ebe4b0c2e61bae92b4eca0d2f4454776
lauichan <te06230623@gmail.com>
2024-01-19 14:57:13 +0900

    컴포넌트 이름 변경, 위치 수정

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,4 +1,40 @@
-function TaskManage({ id, isDone, deleteTask, changeTaskState }) {
+function TaskList({ tasks, children, deleteTask, changeTaskState }) {
+  let rows = [];
+  tasks.forEach((task) => {
+    rows.push(
+      <TaskCard
+        key={task.id}
+        task={task}
+        deleteTask={deleteTask}
+        changeTaskState={changeTaskState}
+      />
+    );
+  });
+
+  return (
+    <section className="todo-section">
+      <h2 className={children.toLowerCase()}>{children}</h2>
+      <ul className="todo-list">{rows}</ul>
+    </section>
+  );
+}
+
+function TaskCard({ task, deleteTask, changeTaskState }) {
+  return (
+    <li>
+      <h3>{task.title}</h3>
+      {task.content}
+      <TaskManageBtn
+        id={task.id}
+        isDone={task.isDone}
+        deleteTask={deleteTask}
+        changeTaskState={changeTaskState}
+      />
+    </li>
+  );
+}
+
+function TaskManageBtn({ id, isDone, deleteTask, changeTaskState }) {
   const taskState = isDone ? "취소" : "완료";
 
   const deleteClickHandler = () => {
@@ -22,42 +58,6 @@ function TaskManage({ id, isDone, deleteTask, changeTaskState }) {
         {taskState}
       </button>
     </div>
-  );
-}
-
-function TaskRow({ task, deleteTask, changeTaskState }) {
-  return (
-    <li>
-      <h3>{task.title}</h3>
-      {task.content}
-      <TaskManage
-        id={task.id}
-        isDone={task.isDone}
-        deleteTask={deleteTask}
-        changeTaskState={changeTaskState}
-      />
-    </li>
-  );
-}
-
-function TaskList({ tasks, children, deleteTask, changeTaskState }) {
-  let rows = [];
-  tasks.forEach((task) => {
-    rows.push(
-      <TaskRow
-        key={task.id}
-        task={task}
-        deleteTask={deleteTask}
-        changeTaskState={changeTaskState}
-      />
-    );
-  });
-
-  return (
-    <section className="todo-section">
-      <h2 className={children.toLowerCase()}>{children}</h2>
-      <ul className="todo-list">{rows}</ul>
-    </section>
   );
 }
 
